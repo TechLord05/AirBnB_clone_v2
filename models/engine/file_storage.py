58de4e23e56f4c7688a28ed689f57b4cef11603e
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2024-01-19 04:15:30 +0000

    Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 
 class FileStorage:
@@ -49,17 +42,30 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
-
     def reload(self):
-        """Deserialize the file path to JSON file path"""
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
         try:
-            with open(self.__file_path, 'r', encoding="UTF-8") as f:
-                content = f.read()
-                if content:
-                    for key, value in json.loads(content).items():
-                        value = eval(value["__class__"])(**value)
-                        self.__objects[key] = value
-        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
             pass
+
+
 
  
