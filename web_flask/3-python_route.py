b7f4ac51dab9557404ff2c880254e06501ad4aa0
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2024-02-19 07:49:02 +0000

    Python flask is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """define to display what it's returning"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """define to display what it's returning"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """define to display what it's returning"""
+    return "C {}".format(escape(text.replace("_", " ")))
+
+
+@app.route('/python/', defaults={"text": "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """define to display what it's returning"""
+    return "Python {}".format(escape(text.replace("_", " ")))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
